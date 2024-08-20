e43787a9ca9cfd485f13ef629b663701d989f7fe
Ankesh Kumar <ankesh9053@gmail.com>
2024-08-20 17:26:38 +0530

    done

---
+++ Assignments/Node_Assignment/Backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+// Import your routes
+const postRoutes = require('./post');
+const deleteRoute = require('./delete');
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Use the imported routes with relative paths
+app.use('http://localhost:3000', postRoutes);
+app.use('/api', deleteRoute);
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
